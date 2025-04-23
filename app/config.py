12b82ae661ea37f519065005ce4a4e878bdc78b2
RailwayRS <mexanuk09@gmail.com>
2025-04-23 20:29:41 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,17 @@
-
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 @dataclass
 class Settings:
     bot_token: str = os.getenv("BOT_TOKEN", "")
     database_url: str = os.getenv("DATABASE_URL", "")
-    admin_ids: list[int] = [int(x) for x in os.getenv("ADMINS", "").split(",") if x]
+    admin_ids: list[int] = field(
+        default_factory=lambda: [
+            int(x) for x in os.getenv("ADMINS", "").split(",") if x
+        ]
+    )
+
 settings = Settings()
+
