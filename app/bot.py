b61de50d3b30693966885ef6826dd1dcde65c840
RailwayRS <mexanuk09@gmail.com>
2025-04-23 21:46:23 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,4 +1,4 @@
-from app.handlers import applications
+from app.handlers import applications, start  # Добавил импорт start
 
 import asyncio, logging
 from aiogram import Bot, Dispatcher
@@ -6,17 +6,24 @@ from app.config import settings
 from app.handlers import common
 from app.models import Base
 from app.database import engine
+
 async def on_startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 async def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(settings.bot_token, parse_mode="HTML")
     dp = Dispatcher()
+
+    # Подключаем роутеры (хендлеры)
     dp.include_router(common.router)
-    await on_startup()
+    dp.include_router(start.router)  # Подключаем start
     dp.include_router(applications.router)
 
+    await on_startup()
     await dp.start_polling(bot)
+
 if __name__ == "__main__":
     asyncio.run(main())
+
